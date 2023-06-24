b4b06379cd96dc79ff879854f6cbc66aa3c61957
Annazz15 <117827110+Annazz15@users.noreply.github.com>
2023-06-24 11:41:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,25 +40,6 @@ scrollButton.addEventListener("click", function (e) {
   });
 });
 
-function setCookieAccepted() {
-  document.cookie = "cookieAccepted=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/";
-  document.body.classList.add('cookie-accepted');
-}
-
-
-var acceptButton = document.querySelector('.cookie-button:nth-of-type(1)');
-var rejectButton = document.querySelector('.cookie-button:nth-of-type(2)');
-
-acceptButton.addEventListener('click', function() {
-  setCookieAccepted(); 
-  document.querySelector('.cookie-banner').style.display = 'none'; 
-});
-
-rejectButton.addEventListener('click', function() {
-  
-  document.querySelector('.cookie-banner').style.display = 'none'; 
-});
-
 
 if (!isCookieAccepted()) {
   document.querySelector('.cookie-banner').style.display = 'block'; 
